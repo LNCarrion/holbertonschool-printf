54bb89ab7314d2b555c120b768e4a9f73e1733b8
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-11-26 18:31:05 -0400

    Fixing count problems

--- _printf.c
+++ _printf.c
@@ -99,9 +99,7 @@ int _printf(const char *format, ...)
 				case 'i':
 					nl = va_arg(args, long int);
 					count = print_number(nl);
-					if (count != 0)
-						count = count - 1;
-					break;
+					return (count);
 
 				default:
 					putchar('%');
