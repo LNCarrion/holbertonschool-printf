6c7d20dc1fdcd5d6d74fb9843c57536e8d7d13ab
Jahaziel Serrano <serranojahaziel2@gmail.com>
2023-11-26 19:17:48 -0400

    added new function

--- main.h
+++ main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 int _putchar(char c);
-int print_number(long int n);
+int print_number(int number);
+int print_long(long int number)
 int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
