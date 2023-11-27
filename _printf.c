62c3a596e3f3b91db0ef108dc82e223fee6d70e6
lestynomar <lestynomar@penguin>
2023-11-26 23:38:15 -0400

    fixing

--- _printf.c
+++ _printf.c
@@ -2,98 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "main.h"
-
-/**
-<<<<<<< HEAD
- * _printf - Custom printf function.
-=======
- * print_number - a function that prints a number digit by digit
- * @number: Number to be printed
- */
-int print_long(long int number) {
-
-        int i, count = 0;
-
-        if (number < 0)
-        {
-                number = -number;
-		count = count + 1;
-        }
-        if (number == 0)
-        {
-                putchar('0');
-        }
-        else
-        {
-                char digits[20];
-
-                while (number > 0)
-                {
-                        digits[count] = '0' + (number % 10);
-                        number /= 10;
-                        count++;
-                }
-                for (i = count - 1; i >= 0; i--)
-                {
-                        putchar(digits[i]);
-                }
-        }
-
-        return (count);
-}
-
-
-int print_number(int number) {
-	
-	int i, count = 0;
-	
-	if (number < 0)
-	{
-		putchar('-');
-		number = -number;
-		count = 1;
-	}
-	if (number == 0)
-	{
-		putchar('0');
-	} 
-	else 
-	{
-		char digits[20];
-		
-		while (number > 0)
-		{
-			digits[count] = '0' + (number % 10);
-			number /= 10;
-			count++;
-		}
-		for (i = count - 1; i >= 0; i--) 
-		{
-			putchar(digits[i]);
-		}
-	}
-
-	return (count);
-}
-
 
 /**
  * _printf - Custom printf function
->>>>>>> c207f1701960ccecc14c9871c34fabe8eb405afc
  * @format: Format string
  *
  * Return: Number of characters printed (excluding null byte)
  */
-
 int _printf(const char *format, ...)
 {
-	int count = 0, n;
-	long int nl;
+	va_list args;
 	const char *ptr;
 	char c;
-  
-	va_list args;
+	int count = 0;
 
 	va_start(args, format);
 
@@ -105,8 +26,7 @@ int _printf(const char *format, ...)
 			{
 				case 'c':
 					c = va_arg(args, int);
-					putchar(c);
-					count++;
+					putchar(c), count++;
 					break;
 				case 's':
 					{
@@ -114,50 +34,34 @@ int _printf(const char *format, ...)
 						if (str == NULL)
 							str = "(null)";
 						while (*str)
-						{
-							putchar(*str++);
-							count++;
-						}
+							putchar(*str++), count++;
 					}
 					break;
 				case '%':
-					putchar('%');
-					count++;
+					putchar('%'), count++;
 					break;
-          
-        /*If the format is a d is an integer*/
-				case 'd':
-					n = va_arg(args, int);
-					if (n > 0)
-                                        {
-                                                count = count + (print_long(n) - 1);
-                                        }
-                                        else
-                                        {
-                                                count = count + (print_long(n));
-                                        }
+				case 'r':
+					{
+						char *str = va_arg(args, char *);
+						if (str == NULL)
+							str = "(null)";
+
+						int len = strlen(str);
+						for (int i = len - 1; i >= 0; i--)
+							putchar(str[i]), count++;
+					}
 					break;
-
-
-        
-
-
 				default:
-					putchar('%');
-					putchar(*ptr);
-					count += 2;
+					putchar('%'), putchar(*ptr), count += 2;
 					break;
 			}
 		}
 		else
-		{
-			putchar(*ptr);
-			count++;
-		}
+			putchar(*ptr), count++;
 	}
-	va_end(args);
-	count++;
 
-	return (count);
+	va_end(args);
+
+	return count;
 }
 
